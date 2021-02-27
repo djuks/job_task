a0dd8202055d426eeb993213b92fce2d13ab8965
djuks <djuricastrahinja@gmail.com>
2021-02-27 14:03:46 +0100

    regenerate new docs

--- spec/rspec_api_doc_helper.rb
+++ spec/rspec_api_doc_helper.rb
@@ -1,11 +1,10 @@
-# frozen_string_literal: true
-
 require 'rspec_api_documentation'
 require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
   config.format = :html
-  config.request_headers_to_include = %w[Accept Content-Type]
+  config.request_headers_to_include = %w[Accept Content-Type Authorization]
+  config.response_headers_to_include = %w[Content-Type Authorization]
   config.docs_dir = Rails.root.join('public/docs')
   config.disable_dsl_status!
   config.keep_source_order = false
